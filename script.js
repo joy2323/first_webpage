c1d729cb4bd975f3dcf509d5e34d03ca3c66f996
joy2323 <joyjoel2011@gmail.com>
2020-03-24 20:57:15 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$name = 'Oluebube Joel';
+console.log($name);
+
+let courses = [
+    'HTML',
+    'CSS',
+    'JAVASCRIPT',
+];
+console.log(courses);
+
+//  find and display all odd numbers from 1-200 (inclusive)
+for (let i=0; i<200; i++) {
+    if(i % 2 !== 0) {
+        console.log(i);
+    }
+}
